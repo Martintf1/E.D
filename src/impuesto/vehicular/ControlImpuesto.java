ad739e81d4a7baa02dd5e43f69775ca4d43d34bd
M4rtin <M4rtin@DESKTOP-215O0LU>
2025-04-08 12:20:40 -0500

    mini proyecto cambio

--- src/impuesto/vehicular/ControlImpuesto.java
+++ src/impuesto/vehicular/ControlImpuesto.java
@@ -4,7 +4,6 @@
  */
 package impuesto.vehicular;
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -15,46 +14,50 @@ import java.util.Scanner;
 public class ControlImpuesto {
 
     Scanner in = new Scanner(System.in);
-    int numMotor[];
-    ArrayList vehiculos = new ArrayList();
+    int numMotor[]; //arreglo sin tamaño
+    ArrayList vehiculos = new ArrayList(); //lista de vehiculos
 
     void registrar() {
+        in.nextLine();
         System.out.println("...REGISTRO VEHICULO...");
         System.out.print("Digite el numero de placa: ");
         String placa = in.nextLine();
+        in.nextLine();
         System.out.print("Digite el numero del motor: ");
         int numeroMotor = in.nextInt();
         System.out.print("Digite el valor a pagar: ");
         double valorPagar = in.nextDouble();
         System.out.print("Digite el nombre del propietario: ");
+        in.nextLine();
         String propietario = in.nextLine();
         System.out.print("Digite el modelo del vehiculo: ");
         String modelo = in.nextLine();
 
-        Vehiculo objeto = new Vehiculo();
+        Vehiculo objeto = new Vehiculo(); //obj clase vehiculo 
 
         objeto.setPlaca(placa);
         objeto.setNumeroMotor(numeroMotor);
         objeto.setValorPagar(valorPagar);
         objeto.setPropietario(propietario);
-        objeto.setModelo(modelo);
+        objeto.setModelo(modelo); // guardar datos ingresados 
 
-        vehiculos.add(objeto);
+        vehiculos.add(objeto); //agregar  los datos a la lista 
 
         System.out.println("VEHICULO REGISTRADO");
 
     }
 
     void buscarVehiculo() {
+        in.nextLine();
         boolean bandera = false;
 
         System.out.println("...BUSCAR VEHICULO POR NUMERO DE PLACA...");
         System.out.print("Digite placa a buscar: ");
         String placaBuscar = in.nextLine();
 
-        for (int i = 0; i < vehiculos.size(); i++) {
-            Vehiculo recuperar = (Vehiculo) vehiculos.get(i);
-            if (recuperar.getPlaca().equalsIgnoreCase(placaBuscar)) {
+        for (int i = 0; i < vehiculos.size(); i++) { //recorrer toda la lista, .size el tamaño de la lista vehiculo, lista dinamica
+            Vehiculo recuperar = (Vehiculo) vehiculos.get(i);  // ----
+            if (recuperar.getPlaca().equalsIgnoreCase(placaBuscar)) { //comparar ignorando may de min
                 bandera = true;
                 System.out.println("PLACA: " + recuperar.getPlaca() + "\nNUMERO DE MOTOR: " + recuperar.getNumeroMotor() + "\nVALOR A PAGAR:" + recuperar.getValorPagar() + "\nPROPIETARIO:" + recuperar.getPropietario() + "\nMODELO:" + recuperar.getModelo());
             }
@@ -66,14 +69,15 @@ public class ControlImpuesto {
     }
 
     void buscarModelo() {
+        in.nextLine();
         boolean bandera = false;
 
         System.out.println("...BUSCAR VEHICULO POR MODELO...");
         System.out.print("Digite modelo de vehiculo a buscar: ");
         String modeloBuscar = in.nextLine();
 
-        for (int i = 0; i < vehiculos.size(); i++) {
-            Vehiculo recuperar = (Vehiculo) vehiculos.get(i);
+        for (int i = 0; i < vehiculos.size(); i++) { //buscar en la lista 
+            Vehiculo recuperar = (Vehiculo) vehiculos.get(i); //---
             if (recuperar.getModelo().equalsIgnoreCase(modeloBuscar)) {
                 bandera = true;
                 System.out.println("PLACA: " + recuperar.getPlaca() + "\nNUMERO DE MOTOR: " + recuperar.getNumeroMotor() + "\nVALOR A PAGAR:" + recuperar.getValorPagar() + "\nPROPIETARIO:" + recuperar.getPropietario() + "\nMODELO:" + recuperar.getModelo());
@@ -85,43 +89,43 @@ public class ControlImpuesto {
         }
     }
 
-    void guardarNumMotor(){
-        numMotor=new int [vehiculos.size()];
+    void guardarNumMotor() {
+        numMotor = new int[vehiculos.size()];
         for (int i = 0; i < vehiculos.size(); i++) {
-            Vehiculo recuperar = (Vehiculo) vehiculos.get(i);
-            numMotor[i]=recuperar.getNumeroMotor();
+            Vehiculo recuperar = (Vehiculo) vehiculos.get(i); //----
+            numMotor[i] = recuperar.getNumeroMotor();  //guardar los numeros del motor en el arreglo num mtor
         }
         System.out.println("NUMEROS DE MOTOR GUARDADOS");
     }
-    
-    void ordenarporBurbuja(int arreglo[]){
-		int n=arreglo.length;
-		//variable usada pa
-		int aux=0;
-		
-		for (int i = 0; i < n-1; i++) {
-			/*Este ciclo se encraga de llevar el mayor al final del arreglo
+
+    void ordenarporBurbuja(int arreglo[]) {
+        int n = arreglo.length;
+        //variable usada pa
+        int aux = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            /*Este ciclo se encraga de llevar el mayor al final del arreglo
 			 * Va desde 0 hasta se haya ido ornando el mayor
 			 * */
-			for (int j = 0; j < n-i-1; j++) {
-				//en caso de que se cumpla la condicion se intercambia la posicion
-				if(arreglo[j]>arreglo[j+1]){
-					aux=arreglo[j];
-					arreglo[j]=arreglo[j+1];
-					arreglo[j+1]=aux;
-				}
-			}
-			
-		}
-		
-		//Listar el arreglo ordenado
-		System.out.println("\n---Arreglo ordenado por metodo Burbuja----");
-		for (int i : arreglo) {
-			System.out.print(i+"-");
-		}
-		
-	}
-    
+            for (int j = 0; j < n - i - 1; j++) {
+                //en caso de que se cumpla la condicion se intercambia la posicion
+                if (arreglo[j] > arreglo[j + 1]) {
+                    aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                }
+            }
+
+        }
+
+        //Listar el arreglo ordenado
+        System.out.println("\n---Arreglo ordenado por metodo Burbuja----");
+        for (int i : arreglo) {
+            System.out.print(i + "-");
+        }
+
+    }
+
     public void ordenarPorInserccion() {
         int insercion; // variable temporal para contener el elemento a insertar
 
@@ -144,4 +148,87 @@ public class ControlImpuesto {
             //imprimirPasada( siguiente, moverElemento ); // imprime la pasada del algoritmo
         }
     }
+
+    void buscar() {
+        System.out.println("Buscanco...");
+        System.out.print("Digite el numero a buscar: ");
+        int numBuscado = in.nextInt();
+
+        //llamo al metodo del algortimo de busqueda binaria
+        int datoRet = this.busquedaBinaria(numBuscado);
+
+        if (datoRet == -1) {
+            System.out.println("Numero no Encontrado!!");
+        } else {
+            System.out.println("Encontrado. Posicion=" + datoRet);
+        }
+
+    }
+
+    public int busquedaBinaria(int elementoBusqueda) {
+        int inferior = 0; // extremo inferior del �rea de b�squeda
+        int superior = numMotor.length - 1; // extremo superior del �rea de b�squeda
+        int medio = (inferior + superior + 1) / 2; // elemento medio
+        int ubicacion = -1; // devuelve el valor; -1 si no lo encontr�
+
+        do {
+            if (elementoBusqueda == numMotor[medio]) {
+                ubicacion = medio; // la ubicaci�n es el elemento medio actual
+            } // el elemento medio es demasiado alto
+            else if (elementoBusqueda < numMotor[medio]) {
+                superior = medio - 1; // elimina la mitad superior
+            } else // el elemento medio es demasiado bajo
+            {
+                inferior = medio + 1; // elimina la mitad inferior
+            }
+            medio = (inferior + superior + 1) / 2; // recalcula el elemento medio
+        } while ((inferior <= superior) && (ubicacion == -1));
+
+        return ubicacion; // devuelve 
+    }
+
+    void menu() {
+        int opc = 0;
+        do {
+            System.out.println("""
+                               Muenu:
+                               1.registro
+                               2.buscar vehiculo por placa
+                               3. buscar modelo
+                               4. guardar numero de motor
+                               5.ordenar por burbuja
+                               6.ordenar por insercion
+                               7. buscar numero de motorusando busqueda binaria
+                               seleccione una opcion;
+                               """);
+            opc = in.nextInt();
+            switch (opc) {
+                case 1:
+                    this.registrar();
+                    break;
+                case 2:
+                    this.buscarVehiculo();
+                    break;
+                case 3:
+                    this.buscarModelo();
+                    break;
+                case 4:
+                    this.guardarNumMotor();
+                    break;
+                case 5:
+                    this.ordenarporBurbuja(numMotor);
+                    break;
+                case 6:
+                    this.ordenarPorInserccion();
+                    break;
+                case 7:
+                    this.buscar();
+                    this.busquedaBinaria(numMotor.length);
+                    break;
+
+            }
+
+        } while (opc != 8);
+    }
+
 }
--- src/impuesto/vehicular/PruebaImpuesto.java
+++ src/impuesto/vehicular/PruebaImpuesto.java
@@ -15,6 +15,8 @@ public class PruebaImpuesto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ControlImpuesto objcpntrolimpuesto= new ControlImpuesto();
+        objcpntrolimpuesto.menu();
     }
     
 }
